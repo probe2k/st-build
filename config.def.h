e14c3dee4d5d40b0e0e2b458427fa6a9061d97d5
probe2k <kyash054@gmail.com>
2021-06-14 16:14:49 +0530

    w3m patch drop | ueberzug + alpha > picom blur

--- config.def.h
+++ config.def.h
@@ -96,6 +96,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.50;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
